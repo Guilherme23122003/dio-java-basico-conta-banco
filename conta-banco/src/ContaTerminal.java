bb17739be7ddb701ad8cf4e8e5913b79864ff0dc
Guilherme Alves <guilhermepontojesus@gmail.com>
2025-04-15 12:37:00 -0300

    Projeto DIO - ContaTerminal, concluído.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, word!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        //Por favor, digite o Nome Cliente
+        System.out.print("Por favor, digite seu nome: ");
+        String nomeDoCliente = scanner.nextLine();
+
+
+
+        //Por favor, digite o número da conta
+        System.out.print("Por favor, digite o número da conta: ");
+        int numero = scanner.nextInt();
+
+        scanner.nextLine(); //Limpando o buffer
+
+        //Por favor, digite o número da Agência
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+
+
+        //Por favor, informe seu saldo
+        System.out.print("Por favor, informe seu saldo: ");
+        Double saldo = scanner.nextDouble();
+
+
+        System.out.println("Olá " + nomeDoCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+
     }
 }
---
+++ conta-banco/src/Usuario.java
@@ -0,0 +1,8 @@
+public class Usuario {
+    
+    int numero = 1021;
+    String agencia = "067-8";
+    String NomeDoCliente = "Mario Andrade";
+    Double Saldo = 237.48;
+
+}
