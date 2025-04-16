3b3b396bc0481bcad151a447e64a43f74270a60b
Guilherme Alves <guilhermepontojesus@gmail.com>
2025-04-16 15:53:47 -0300

    Atualização dos arquivos, praticas de estudos

---
+++ conta-banco/ExemploArrayFor.java
@@ -0,0 +1,11 @@
+public class ExemploArrayFor {
+
+    public static void main(String[] args) {
+        String[] alunos = {"Guilherme", "João", "Jonas"};
+
+        for (String aluno : alunos) {
+            System.out.println("Nome do aluno é " + aluno);
+        }
+    }
+
+}
