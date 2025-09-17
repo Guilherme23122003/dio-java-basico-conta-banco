aceba18cbad39db2a2d351d16707618128e1f119
Guilherme Alves <guilhermepontojesus@gmail.com>
2025-09-17 16:00:01 -0300

    melhorias no ContaTerminal

--- conta-banco/src/Usuario.java
+++ conta-banco/src/Usuario.java
@@ -1,8 +1,66 @@
 public class Usuario {
-    
-    int numero = 1021;
-    String agencia = "067-8";
-    String NomeDoCliente = "Mario Andrade";
-    Double Saldo = 237.48;
+    private String nomeDoCliente;
+    private String cpf;
+    private int numeroDaConta;
+    private int agencia;
+    private Double Saldo;
 
+    private static int contadorAgencia = 1;
+    private static int contadorConta = 2;
+
+    public Usuario(String nomeDoCliente, String cpf, Double saldo) {
+        this.nomeDoCliente = nomeDoCliente;
+        this.cpf = cpf;
+        this.numeroDaConta = contadorConta++;
+        this.agencia = contadorAgencia++;
+        Saldo = saldo;
+    }
+
+    public String getNomeDoCliente() {
+        return nomeDoCliente;
+    }
+
+    public void setNomeDoCliente(String nomeDoCliente) {
+        this.nomeDoCliente = nomeDoCliente;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getNumeroDaConta() {
+        return numeroDaConta;
+    }
+
+    public void setNumeroDaConta(int numeroDaConta) {
+        this.numeroDaConta = numeroDaConta;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public Double getSaldo() {
+        return Saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        Saldo = saldo;
+    }
+
+    public static int getContadorAgencia() {
+        return contadorAgencia;
+    }
+
+    public static void setContadorAgencia(int contadorAgencia) {
+        Usuario.contadorAgencia = contadorAgencia;
+    }
 }
